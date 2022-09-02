ce601a6d0b49848a5ba5136812e2f495d870e86c
SagarR.B <sagarrb1805@gmail.com>
2022-09-02 09:13:15 +0530

    shopping cart sample

---
+++ shopping/search_app/views.py
@@ -0,0 +1,15 @@
+from itertools import product
+from django.shortcuts import render
+from shoppingapp.models import Product
+# Create your views here.
+from django.db.models import Q
+
+
+def SearchResult(request):
+    products = None
+    query = None
+    if 'q' in request.GET:
+        query = request.GET.get('q')
+        products = Product.objects.filter(Q(name__contains=query) | Q(description__contains=query))
+
+    return render(request, 'search.html', {'query':query, 'products':products})
